3f84723b26af311479bb02c30e7a4da7b3356c4d
Furkan Demirtaş <furkan-demirtas@outlook.com>
2024-12-21 14:59:13 +0300

    StrictMode kaldırıldı

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./assets/css/reset.css";
 import "./assets/css/main.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <App />
+  // </StrictMode>
 );
