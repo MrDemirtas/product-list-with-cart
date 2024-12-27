649c93a9278b2196e33a5c9c7d50ea4e636c5d85
Furkan Demirtaş <furkan-demirtas@outlook.com>
2024-12-28 00:58:17 +0300

    cart etkileşimi eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,122 @@
-import { product } from "./assets/data/data.jsx";
+import { product } from "./assets/data/data";
 import { useState } from "react";
+
 function App() {
+  const [cart, setCart] = useState([]);
+  
+  function handleAddToCart(params) {
+    if (cart.find((item) => item.id === params.id)) {
+      setCart(
+        cart.map((item) => {
+          if (item.id === params.id) {
+            return { ...item, count: item.count + 1 };
+          } else {
+            return item;
+          }
+        })
+      );
+    } else {
+      setCart([...cart, { count: 1, ...params }]);
+    }
+  }
+
+  function handleRemoveCart(params) {
+    if (cart.find((item) => item.id === params.id).count > 1) {
+      setCart(
+        cart.map((item) => {
+          if (item.id === params.id) {
+            return { ...item, count: item.count - 1 };
+          } else {
+            return item;
+          }
+        })
+      );
+    } else {
+      setCart(cart.filter((item) => item.id !== params.id));
+    }
+  }
+
+  function getTotal() {
+    let total = 0;
+    cart.map((item) => (total += item.price * item.count));
+    return total.toFixed(2);
+  }
+
   return (
     <div className="container">
       <div className="product-list">
         <h1>Desserts</h1>
         <div className="product-list-items">
           {product.map((item) => (
-            <ProductCard product={item} key={item.id} />
+            <ProductCard product={item} key={item.id} handleAddToCart={handleAddToCart} handleRemoveCart={handleRemoveCart} cart={cart} />
           ))}
         </div>
       </div>
       <div className="cart">
-        <h1>Your Cart (0)</h1>
-        <div className="cart-items"></div>
+        <h1>Your Cart ({cart.length})</h1>
+        <div className="cart-items" style={cart.length === 0 ? { background: "url('/images/cart-bg.svg') no-repeat center center" } : {}}>
+          {cart.map((item) => (
+            <div className="cart-item" key={item.id}>
+              <div className="product-info">
+                <h2>{item.name}</h2>
+                <div className="product-price">
+                  <p className="product-count">x{item.count}</p>
+                  <p className="product-price">@ ${item.price.toFixed(2)}</p>
+                  <p className="product-total">${(item.price * item.count).toFixed(2)}</p>
+                </div>
+              </div>
+              <button onClick={() => handleRemoveCart(item)}>
+                <img src="/images/remove.svg" alt="remove" />
+              </button>
+            </div>
+          ))}
+          {cart.length !== 0 && (
+            <div className="cart-total">
+              <div className="order-total">
+                <span>Order Total</span>
+                <span className="total-price">${getTotal()}</span>
+              </div>
+              <div className="carbon-neutral">
+                <img src="/images/carbon_tree.svg" alt="carbon-neutral" />
+                <p>This is a <strong>carbon-neutral</strong> delivery</p>
+              </div>
+              <button className="confirm-order">Confirm Order</button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 }
 
-function ProductCard({ product }) {
-  const [count, setCount] = useState(0);
+function ProductCard({ product, handleAddToCart, handleRemoveCart, cart }) {
+  function handleAdd() {
+    handleAddToCart(product);
+  }
+
+  function handleRemove() {
+    handleRemoveCart(product);
+  }
+
+  const isInCart = cart.find((item) => item.id === product.id);
 
   return (
     <div className="card">
       <figure>
         <img src={product.image} alt={product.name} />
-        {count > 0 ? (
+        {isInCart ? (
           <div className="cart-count">
-            <button onClick={() => setCount(count - 1)}>
+            <button onClick={handleRemove}>
               <img src="/images/minus.svg" alt="minus" />
             </button>
-            <span>{count} </span>
-            <button onClick={() => setCount(count + 1)}>
+            <span>{isInCart.count} </span>
+            <button onClick={handleAdd}>
               <img src="/images/plus.svg" alt="plus" />
             </button>
           </div>
         ) : (
           <div className="cart-button">
-            <button onClick={() => setCount(count + 1)}>
+            <button onClick={handleAdd}>
               <img src="/images/cart.svg" alt="cart" />
               Add to Cart
             </button>
